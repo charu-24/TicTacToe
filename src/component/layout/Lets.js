1e4f30cb23ff815d05f87445559d7ec9d77c4eab
Charu <charurampur.1999@gmail.com>
2020-04-08 00:26:03 +0530

    Updated Lets.js

--- src/component/layout/Lets.js
+++ src/component/layout/Lets.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Lets() {
     return (
         <div style={buttonStyle}>
+            <Link to="/about" >
             <button style={{width:"200px", padding: "10px",marginLeft:"-10%",backgroundColor:"#0f2862", borderColor:"black",boxShadow:"2px 5px 6px black",
-        borderRadius:"30px",color:"#ffa800",fontStyle:"bold"}}>Let's Begin</button>
+            borderRadius:"30px",color:"#ffa800",fontStyle:"bold"}}>Let's Begin</button>
+            </Link>
+            
         </div>
     )
 }
